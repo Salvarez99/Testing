45e57e0c1303726601e98ac21b540d9db9fc17cd
Salvarez99 <Stephenalvarez3@gmail.com>
2023-09-27 22:02:38 -0400

    stuff

--- hello.py
+++ hello.py
@@ -5,3 +5,9 @@ print("goodbye")
 #I added this comment in branch_1, added this to the comment
 # I added this comment in the main branch
 #branch_2 here, hello
+
+num = [2,3,4,5]
+
+sum = add(num)
+
+print(sum)
